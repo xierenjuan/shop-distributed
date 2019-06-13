edb05aea0d77f0e36f16cc7ff2516dce4fb56c12
xierenjuan <895591554@qq.com>
2019-06-13 13:57:59 +0800

    桥接模式

---
+++ maven-root/goods-service/src/main/java/com/test/maven/goods/designpatterns/桥接模式/Implementor.java
@@ -0,0 +1,8 @@
+package com.test.maven.goods.designpatterns.桥接模式;
+
+public abstract class Implementor {
+    /**
+     * 示例方法，实现抽象部分需要的某些具体功能
+     */
+    public abstract void operationImpl();
+}
---
+++ maven-root/goods-service/src/main/java/com/test/maven/goods/designpatterns/桥接模式/RefinedAbstraction.java
@@ -0,0 +1,12 @@
+package com.test.maven.goods.designpatterns.桥接模式;
+
+public class RefinedAbstraction extends Abstraction {
+    
+    public RefinedAbstraction(Implementor impl) {
+        super(impl);
+    }
+    //其他的操作方法
+    public void otherOperation(){
+        
+    }
+}
