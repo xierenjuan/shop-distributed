f151bb506f0de8c05f33388a86751ac74f1670c4
YL <895591554@qq.com>
2021-06-09 15:36:32 +0800

    order-service去掉从配置中心读取文件

--- shop-order/order-service/src/main/java/com/test/maven/order/server/OrderServiceApplication.java
+++ shop-order/order-service/src/main/java/com/test/maven/order/server/OrderServiceApplication.java
@@ -2,6 +2,7 @@ package com.test.maven.order.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -12,6 +13,7 @@ import springfox.documentation.oas.annotations.EnableOpenApi;
 @EnableEurekaClient
 @EnableFeignClients(basePackages = "com.test.maven.*.api")
 @EnableOpenApi
+@EnableDiscoveryClient
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
--- shop-order/order-service/src/main/java/com/test/maven/order/server/controller/OrderController.java
+++ shop-order/order-service/src/main/java/com/test/maven/order/server/controller/OrderController.java
@@ -6,9 +6,13 @@ import com.test.maven.order.server.service.OrderService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Api("订单相关")
 @RestController
@@ -17,10 +21,27 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @ApiOperation("查询数据库")
     @GetMapping("/queryDatabase")
     public CommonResult<Object> queryDatabase(){
         return CommonResult.getSucceedInstance(orderService.list());
     }
 
+    @GetMapping(value = "/discovery")
+    public Object discovery(){
+        List<String> services = discoveryClient.getServices();
+        for (String element : services) {
+            System.out.println(element);
+        }
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance element : instances) {
+            System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+                    + element.getUri());
+        }
+        return this.discoveryClient;
+    }
 }
